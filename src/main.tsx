227a7454ba18f9d491af701e448c7a0ccd81854c
BudidhaBhanuChander <bunnybhanu863@gmail.com>
2026-06-21 20:55:13 +0530

    feat: cart badge, favorites wishlist, coupons, real reviews

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
+import { FavoritesProvider } from "./context/FavoritesContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
         <AuthProvider>
             <CartProvider>
-                <App />
+                <FavoritesProvider>
+                    <App />
+                </FavoritesProvider>
             </CartProvider>
         </AuthProvider>
     </BrowserRouter>
